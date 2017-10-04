ccce216fa4c383cd3435a97293e82349543fab55
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2017-10-04 15:24:13 -0500

    added the djcat_lab.py

---
+++ djcat_lab.py
@@ -0,0 +1,56 @@
+@schema
+class Keyword(dj.Lookup):
+    definition = """
+    # Tag of study types
+    keyword : varchar(24)  
+    """
+    contents = zip(['behavior', 'extracellular', 'photostim'])
+
+@schema
+class Study(dj.Manual):
+    definition = """
+    # Study 
+    study : varchar(8)    # short name of the study
+    --- 
+    study_description : varchar(255)   #  
+    institution : varchar(255)  # institution conducting the study
+    lab : varchar(255)  #  lab conducting the study
+    reference_atlas : varchar(255)   # e.g. "paxinos"
+    """
+    
+@schema
+class StudyKeyword(dj.Manual):
+    definition = """
+    # Study keyword (see general/notes)
+    -> Study
+    -> Keyword
+    """
+
+@schema
+class Publication(dj.Manual):
+    definition = """
+    # Publication
+    doi  : varchar(60)   # publication DOI
+    ----
+    full_citation : varchar(4000)
+    authors='' : varchar(4000)
+    title=''   : varchar(1024)
+    """
+    
+@schema
+class RelatedPublication(dj.Manual):
+    definition = """
+    -> Study
+    -> Publication
+    """
+
+@schema
+class Subject(dj.Manual):
+    definition = """
+    subject_id  : int   # institution animal ID  
+    --- 
+    species        : varchar(30)
+    date_of_birth  : date   
+    sex            : enum('M','F','Unknown')
+    animal_source  : varchar(30)
+    """
